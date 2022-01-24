85279c80dd36c7de510e832dd99deec737dc912a
guoruize <guoruize@360.cn>
2022-01-24 16:33:18 +0800

    增加Inherited_widget 练习

---
+++ lib/pages/name_game.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:two_you_friend/inherited_widget/name_inherited_widget.dart';
+import 'package:two_you_friend/widgets/name_game/random_name.dart';
+import 'package:two_you_friend/widgets/name_game/test_other.dart';
+import 'package:two_you_friend/widgets/name_game/welcome.dart';
+
+/// 测试随机名字游戏组件
+class NameGame extends StatefulWidget {
+  /// 构造函数
+  const NameGame({Key? key}) : super(key: key);
+
+  @override
+  createState() => NameGameState();
+}
+
+/// 随机名字游戏组件状态管理类
+class NameGameState extends State<NameGame> {
+  /// name 状态
+  late String name;
+
+  /// 构造函数参数，避免父组件状态变化，而引起的子组件的重 build 操作
+  late Widget child;
+
+  /// 修改当前名字
+  void changeName() {
+    List<String> nameList = ['flutter one', 'flutter two', 'flutter three'];
+    int pos = Random().nextInt(3);
+    setState(() {
+      name = nameList[pos];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  /// 构造函数
+  NameGameState() {
+    name = 'test flutter';
+    child = Column(children: <Widget>[
+      Welcome(),
+      RandomName(),
+      TestOther(),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NameInheritedWidget(child: child, onNameChange: changeName, name: name),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/name_game/welcome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:two_you_friend/inherited_widget/name_inherited_widget.dart';
+
+/// 欢迎人展示组件
+class Welcome extends StatelessWidget {
+  /// 有状态类返回组件信息
+  @override
+  Widget build(BuildContext context) {
+    print('welcome build');
+    final name = NameInheritedWidget.of(context)?.name;
+
+    return Text('欢迎 $name');
+  }
+}
